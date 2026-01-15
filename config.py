f095baaf2092e3cfe909bc8d09b34ce3a09659f0
Iza Carlos <carlos1311ac@gmail.com>
2026-01-14 21:46:19 -0600

    Forgot to change the token (again)

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Environment(str, Enum):
 
 class Config:
     def __init__(self):
-        self.DISCORD_TOKEN = self._get_required("BETA_TOKEN")
+        self.DISCORD_TOKEN = self._get_required("DISCORD_TOKEN")
         self.GUI_TEST_MODE = os.getenv("GUI_TEST_MODE", "false").lower() == "true"
         self.ENVIRONMENT = Environment(os.getenv("ENVIRONMENT", "development"))
         
